2d034382dfe114e9248a5d9859062e137d93fe2e
Hunter Ledet <hunter.ledet@gmail.com>
2017-12-18 14:44:00 -0600

    change in server - heroku deployment

--- server/index.js
+++ server/index.js
@@ -19,9 +19,10 @@ app.get('/items', function (req, res) {
   });
 });
 
-app.get('/', function(req, res) {
+app.get('/', function(req, res){
   res.send('hello world')
 })
+
 
 app.listen(3000, function() {
   console.log('listening on port 3000!');
